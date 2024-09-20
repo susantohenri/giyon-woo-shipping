cdfe9248aeb45a3b7290810d2d78a0c7225e9e4e
Henri Susanto <henry.dinus@gmail.com>
2024-09-20 08:53:53 +0700

    prod styling

--- giyon-woo-shipping-arrival.php
+++ giyon-woo-shipping-arrival.php
@@ -1,36 +1,23 @@
-<fieldset class="wc-block-checkout__shipping-option wp-block-woocommerce-checkout-arrival-methods-block wc-block-components-checkout-step" id="shipping-option">
-    <legend class="screen-reader-text">Jam Kedatangan
-    </legend>
-    <div class="wc-block-components-checkout-step__heading">
-        <h2 class="wc-block-components-title wc-block-components-checkout-step__title" aria-hidden="true">Jam Kedatangan
-        </h2>
-    </div>
-    <div class="wc-block-components-checkout-step__container">
-        <div class="wc-block-components-checkout-step__content">
-            <div class="wc-block-components-notices">
-            </div>
-            <div class="wc-block-components-notices__snackbar wc-block-components-notice-snackbar-list" tabindex="-1">
-                <div></div>
-            </div>
-            <div class="">
-                <div class="" aria-hidden="false">
-                    <div class="wc-block-components-shipping-rates-control css-0 e19lxcc00">
-                        <div class="wc-block-components-shipping-rates-control__package wc-block-components-shipping-rates-control__package--last-selected">
-                            <div class="wc-block-components-radio-control wc-block-components-radio-control--highlight-checked">
-                                <?php foreach (['8:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '19:00 - 21:00', '20:00 - 21:00'] as $hour): ?>
-                                    <label class="wc-block-components-radio-control__option" for="<?= str_replace(' ', '_', $hour) ?>">
-                                        <input id="<?= str_replace(' ', '_', $hour) ?>" class="wc-block-components-radio-control__input" type="radio" name="giyon_arrival_options" aria-describedby="<?= str_replace(' ', '_', $hour) ?>__label <?= str_replace(' ', '_', $hour) ?>__secondary-label" aria-disabled="false" value="<?= $hour ?>">
-                                        <span id="<?= str_replace(' ', '_', $hour) ?>__label" class="wc-block-components-radio-control__label">
-                                            <?= $hour ?>
-                                        </span>
-                                    </label>
-                                <?php endforeach ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
+<fieldset class="wd-table-wrapper wc-block-checkout__shipping-option wp-block-woocommerce-checkout-arrival-methods-block wc-block-components-checkout-step" id="shipping-option">
+    <table class="shop_table">
+        <thead>
+            <tr>
+                <th colspan="2" style="text-align: left;">Jam Kedatangan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="text-align: left;">
+                    <?php foreach (['8:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '19:00 - 21:00', '20:00 - 21:00'] as $hour): ?>
+                        <label class="wc-block-components-radio-control__option" for="<?= str_replace(' ', '_', $hour) ?>">
+                            <input id="<?= str_replace(' ', '_', $hour) ?>" class="wc-block-components-radio-control__input" type="radio" name="giyon_arrival_options" aria-describedby="<?= str_replace(' ', '_', $hour) ?>__label <?= str_replace(' ', '_', $hour) ?>__secondary-label" aria-disabled="false" value="<?= $hour ?>" style="margin-inline-end:0;">
+                            <span id="<?= str_replace(' ', '_', $hour) ?>__label" class="wc-block-components-radio-control__label">
+                                <?= $hour ?>
+                            </span>
+                        </label>
+                    <?php endforeach ?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </fieldset>
